eca7a833e201298257a9c943945cbe0e6365ba19
Yaschilal Enyew <yilkalenyew092@gmail.com>
2025-08-11 02:18:06 +0300

    Update app.js
    change the origin of the frontend

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://smart-auth-frontend-nine.vercel.app/',
   credentials: true
 }));
 app.use(cookieParser());
